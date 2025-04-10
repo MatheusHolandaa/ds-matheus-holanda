8220f6a7c8552645035a58e0a8fadf9f026e0609
Matheus Holanda <matheusdemoura2009@gmail.com>
2025-04-10 11:40:43 -0300

    subindo atividades

---
+++ Laçosrep/src/laçosrep/ex7.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package laçosrep;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class ex7 {
+
+   public static void main(String[] args) {
+        // Laço for para imprimir os números de 1 a 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ Laçosrep/src/laçosrep/ex8.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package laçosrep;
+
+/**
+ *
+ * @author CAMARGO
+ */
+public class ex8 {
+
+   public static void main(String[] args) {
+        // Laço for para imprimir os números de 10 a 1
+        for (int i = 10; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
