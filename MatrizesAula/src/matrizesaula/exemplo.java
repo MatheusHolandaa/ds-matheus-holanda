d9e3081b2656828c35abb3dcb7f3d7d965a48258
Matheus Holanda <matheusdemoura2009@gmail.com>
2025-04-14 09:21:05 -0300

    Exemplos de Aula

---
+++ MatrizesAula/src/matrizesaula/exemplo.java
@@ -0,0 +1,14 @@
+
+package matrizesaula;
+
+
+public class exemplo {
+
+    
+    public static void main(String args[]) {
+        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+cars[0] = "Opel";
+System.out.println(cars[0]);
+// Now outputs Opel instead of Volvo
+    }
+}
